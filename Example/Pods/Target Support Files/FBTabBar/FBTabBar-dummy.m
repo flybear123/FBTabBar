d6a4aad2df20e4d1aabac307c21492238e223d90
flybear123 <liwxiong@126.com>
2021-02-22 14:58:16 +0800

    initial

---
+++ Example/Pods/Target Support Files/FBTabBar/FBTabBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FBTabBar : NSObject
+@end
+@implementation PodsDummy_FBTabBar
+@end
---
+++ Example/Pods/Target Support Files/Pods-FBTabBar_Tests/Pods-FBTabBar_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FBTabBar_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FBTabBar_Tests
+@end
